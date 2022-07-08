fac1885f2dd9fdcf475221b07b9074d570d304b7
jacob.swartzentruber <jsswart@gmail.com>
2022-07-08 16:24:24 -0500

    Deleting now updates page
    Ability to press enter on adding comments now works

--- public/client.js
+++ public/client.js
@@ -26,25 +26,33 @@ $( document ).ready(function() {
       $.each(data.comments, function(i, val) {
         comments.push('<li>' +val+ '</li>');
       });
-      comments.push('<br><form id="newCommentForm"><input style="width:300px" type="text" class="form-control" id="commentToAdd" name="comment" placeholder="New Comment"></form>');
-      comments.push('<br><button class="btn btn-info addComment" id="'+ data._id+'">Add Comment</button>');
-      comments.push('<button class="btn btn-danger deleteBook" id="'+ data._id+'">Delete Book</button>');
+      comments.push('<br><form id="newCommentForm"><input style="width:300px" type="text" class="form-control" id="commentToAdd" name="comment" placeholder="New Comment">');
+      comments.push('<br><button type="submit" value="Submit" class="btn btn-info addComment" id="'+ data._id+'">Add Comment</button>');
+      comments.push('<button class="btn btn-danger deleteBook" id="'+ data._id+'">Delete Book</button></form>');
       $('#detailComments').html(comments.join(''));
     });
   });
-  
-  $('#bookDetail').on('click','button.deleteBook',function() {
+
+  $('#bookDetail').on('click','button.deleteBook',function(event) {
+    event.preventDefault();
     $.ajax({
       url: '/api/books/'+this.id,
       type: 'delete',
       success: function(data) {
         //update list
-        $('#detailComments').html('<p style="color: red;">'+data+'<p><p>Refresh the page</p>');
+        $('#detailComments').html('<p style="color: red;">'+data+'<p>');
+        let id = $('#detailTitle').text().match(/(?<=\(id: )\w*(?=\))/g)[0];
+        let index = itemsRaw.findIndex(book => id === book._id);
+        items.splice(index, 1);
+        itemsRaw.splice(index, 1);
+        $('.listWrapper').html(items.join(''));
       }
     });
   });  
   
-  $('#bookDetail').on('click','button.addComment',function() {
+  $('#bookDetail').on('click','button.addComment',function(event) {
+    event.preventDefault();
+
     let  newComment = $('#commentToAdd').val();
     $.ajax({
       url: '/api/books/'+this.id,
@@ -82,7 +90,6 @@ $( document ).ready(function() {
           });
         
         if (items.length < 15) {
-          console.log(items.length);
           items.push('<li class="bookItem" id="' + items.length + '">' + data.title + ' - 0 comments</li>');
         }
         else if (items.length == 15){
@@ -90,8 +97,6 @@ $( document ).ready(function() {
         }
         else {
           let num = parseInt(items[15].match(/\d+/)[0]) + 1;
-          console.log(num);
-          console.log(items[15]);
           items[15] = '<p>...and '+ num +' more!</p>';
         }
 
@@ -104,10 +109,12 @@ $( document ).ready(function() {
     $.ajax({
       url: '/api/books',
       type: 'delete',
-      dataType: 'json',
-      data: $('#newBookForm').serialize(),
       success: function(data) {
         //update list
+        items = [];
+        itemsRaw = [];
+        $('#detailComments').html('<p style="color: red;">'+data+'<p>');
+        $('.listWrapper').html(items.join(''));
       }
     });
   }); 
